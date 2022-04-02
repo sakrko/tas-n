6c6e4680eb771fa518bbeba91ab990a16bd4ff61
namedsaikrishna <saikrishna.k495@gmail.com>
2022-04-02 15:06:07 +0530

    gh-pages update

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -10,7 +10,10 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import utils.PropHelper;
 
-@Parameters({"tag"})
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @CucumberOptions(features = "src/test/resources/features", glue = {"steps"}, tags = "@REG", plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, monochrome = true, publish = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
     private static final Logger logger = LogManager.getLogger(TestRunner.class);
@@ -24,6 +27,21 @@ public class TestRunner extends AbstractTestNGCucumberTests {
     @Parameters({"browserName", "environment"})
     @BeforeTest
     public void setEnvironment(String browserName, @Optional("stage") String environment) {
+        String dir = System.getProperty("user.dir") + "/Reports/Screenshots";
+        if (Files.exists(Path.of(dir))) {
+            File file = new File(dir);
+            File[] files = file.listFiles();
+            if (!(files == null)) {
+                for (File f : files) {
+                    if (f.getName().endsWith(".png")) {
+                        boolean deleteStatus = f.delete();
+                        if (!deleteStatus) {
+                            logger.error("Failed to delete existing png files");
+                        }
+                    }
+                }
+            }
+        }
         PropHelper.setBrowserName(browserName);
         PropHelper.setEnvironment(environment);
         logger.info("Environment is set");
